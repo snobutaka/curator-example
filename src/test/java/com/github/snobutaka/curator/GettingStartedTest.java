ca93d2c510d9826cc636eb29bf2a99cc999c4647
Nobutaka SAITO <saito.n.ndpmsmeugd@gmail.com>
2018-01-16 22:07:53 +0900

    Use TestingServer (from curator-test) instead of GenericContainer in tests.

--- src/test/java/com/github/snobutaka/curator/GettingStartedTest.java
+++ src/test/java/com/github/snobutaka/curator/GettingStartedTest.java
@@ -3,6 +3,7 @@ package com.github.snobutaka.curator;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -14,27 +15,30 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import org.apache.curator.framework.CuratorFramework;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
+import org.apache.curator.test.TestingServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.testcontainers.containers.GenericContainer;
 
 import com.github.snobutaka.curator.GettingStarted.LockedProcedure;
 
 public class GettingStartedTest {
 
-    private static final int zkPort = 2181;
-    private static String zkAddr;
+    TestingServer zookeeperServer;
 
-    @ClassRule
-    public static GenericContainer ZK =
-        new GenericContainer<>("zookeeper:3.4.11")
-            .withExposedPorts(zkPort);
-
-    @BeforeClass
-    public static void setUpBeforeClass() {
-        zkAddr = "localhost:" + ZK.getMappedPort(zkPort);
+    @Before
+    public void setUp() throws Exception {
+        this.zookeeperServer = new TestingServer();
+        this.zookeeperServer.start();
     }
+
+    @After
+    public void tearDown() throws IOException {
+        if (this.zookeeperServer != null) {
+            this.zookeeperServer.close();
+        }
+    }
+
     /**
      * Test ZooKeeper 3.4.x compatibility.
      * http://curator.apache.org/zk-compatibility.html
@@ -42,21 +46,21 @@ public class GettingStartedTest {
     @Test
     public void testCompatibilityMode() {
         GettingStarted gettingStarted = new GettingStarted();
-        CuratorFramework curator = gettingStarted.getConnection(zkAddr);
+        CuratorFramework curator = gettingStarted.getConnection(this.zookeeperServer.getConnectString());
         assertThat(curator.isZk34CompatibilityMode(), is(true));
     }
 
     @Test
     public void testGetConnection() {
         GettingStarted gettingStarted = new GettingStarted();
-        CuratorFramework curator = gettingStarted.getConnection(zkAddr);
+        CuratorFramework curator = gettingStarted.getConnection(this.zookeeperServer.getConnectString());
         assertThat(curator.isStarted(), is(true));
     }
 
     @Test
     public void testCreate() throws Exception {
         GettingStarted gettingStarted = new GettingStarted();
-        CuratorFramework curator = gettingStarted.getConnection(zkAddr);
+        CuratorFramework curator = gettingStarted.getConnection(this.zookeeperServer.getConnectString());
         String path = "/getting_started_with_curator";
         byte[] data = "Hello Curator!".getBytes(StandardCharsets.UTF_8);
         String ret = gettingStarted.create(curator, path, data);
@@ -70,9 +74,9 @@ public class GettingStartedTest {
         ExecutorService executor = Executors.newFixedThreadPool(2);
 
         final GettingStarted gettingStarted1 = new GettingStarted();
-        final CuratorFramework curator1 = gettingStarted1.getConnection(zkAddr);
+        final CuratorFramework curator1 = gettingStarted1.getConnection(this.zookeeperServer.getConnectString());
         final GettingStarted gettingStarted2 = new GettingStarted();
-        final CuratorFramework curator2 = gettingStarted2.getConnection(zkAddr);
+        final CuratorFramework curator2 = gettingStarted2.getConnection(this.zookeeperServer.getConnectString());
 
         final List<String> target = Collections.synchronizedList(new ArrayList<String>());
 
